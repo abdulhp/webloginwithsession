3c0bc5b19c6ca68369d1c1af9cb7fa61ef681b27
abdulhp <newmarkbeatbox@gmail>
2017-08-30 21:45:20 +0700

    v2

--- proseslogin.php
+++ proseslogin.php
@@ -3,28 +3,26 @@ $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
 $dbname = 'websession';
-//Mendeklarasikan akun mysql di xamp beserta database
 
-$connect = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die('koneksi gagal');// untuk konek ke mysql dan database
+$connect = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die('koneksi gagal');
 
-$uname = $_POST['uname']; // mengambil value uname dari halaman login dan memasukkan nya ke variable $uname
-$pswd     = $_POST['pswd']; // mengambil value pswd dari halaman login dan memamsukkan nya ke variable $pswd
+$uname = $_POST['uname'];
+$pswd     = $_POST['pswd'];
 
 $login = mysqli_query($connect, "SELECT * FROM account WHERE uname = '$uname' AND pswd='$pswd'");
-//untuk melaksanakan query pada koneksi variable $connect, dan menampilkan field uname dan pswd yang sama dengan variable $uname dan pswd diatas
+
 $row=mysqli_fetch_array($login);
-//untuk menampung hasil data field yang telah ditampilkan tadi di array
+
 if ($row['uname'] == $uname AND $row['pswd'] == $pswd)
-	//jika array $row yang field nya uname sama dengan $uname dan sama seperti pswd, maka
+	
 {
 	session_start();
-	// aktifkan session
+	
 	$_SESSION['uname'] = $row['uname'];
 	$_SESSION['pswd'] = $row['pswd'];
-	// mengisi session value uname dan pswd, dengan arrayy $row yang telah didapat tadi
+	
 	header('location:index.php');
-	// autolink ke index.php
-}
+	}
 else
 {
 	echo "<center><br><br><br><br><br><br><b>LOGIN GAGAL! </b><br>
